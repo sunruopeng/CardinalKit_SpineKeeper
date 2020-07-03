17ba7e9272e2d02fba8ac0ad814c57b7ab534c4d
m-junaid-butt <junaidbutt.dev@gmail.com>
2020-07-03 19:26:59 +0500

    - Fix issue of tasks which were not showing on activity screen  - Added two carekit tasks on activity screen.

--- ORKSample/CareStoreReferenceManager.swift
+++ ORKSample/CareStoreReferenceManager.swift
@@ -7,14 +7,15 @@
 //
 
 import Foundation
+import UIKit
 import CareKit
 
 // Singleton wrapper to hold a reference to OCKSynchronizedStoreManager and task identifiers
 final class CareStoreReferenceManager {
     
     enum TaskIdentifiers: String, CaseIterable {
-        case coughingEpisodes
-        case flueEpisodes
+        case exercisesTypes
+        case strengthening
     }
     
     
@@ -22,11 +23,13 @@ final class CareStoreReferenceManager {
     
     // Manages synchronization of a CoreData store
     lazy var synchronizedStoreManager: OCKSynchronizedStoreManager = {
-        let store = OCKStore(name: "Activity")
+        let store = OCKStore(name: "Daily Activity")
         store.populateDailyTasks()
         let manager = OCKSynchronizedStoreManager(wrapping: store)
         return manager
     }()
+    
+    
     
     private init() {}
 }
@@ -39,41 +42,46 @@ extension OCKStore {
     func populateDailyTasks() {
 
         let thisMorning = Calendar.current.startOfDay(for: Date())
-        guard let beforeBreakfast = Calendar.current.date(byAdding: .day, value: 6, to: thisMorning) else {
+        guard let beforeBreakfast = Calendar.current.date(byAdding: .day, value: 0, to: thisMorning) else {
             return assertionFailure("Could not create time 8AM this morning")
         }
 
-        let coughingSchedule = OCKSchedule(composing: [
+        let excerciseSchedule = OCKSchedule(composing: [
             OCKScheduleElement(start: beforeBreakfast,
                                end: nil,
                                interval: DateComponents(day: 1),
-                               text: "Anytime throughout the day",
+                               text: "Read info",
                                targetValues: [],
                                duration: .allDay)
         ])
 
-        var coughingTask = OCKTask(id: CareStoreReferenceManager.TaskIdentifiers.coughingEpisodes.rawValue,
-                                   title: "Track coughing",
-                                   carePlanID: nil, schedule: coughingSchedule)
+        var excerciseTask = OCKTask(id: CareStoreReferenceManager.TaskIdentifiers.exercisesTypes.rawValue,
+                                   title: "Types of Excercise",
+                                   carePlanID: nil, schedule: excerciseSchedule)
         
         
-        let flueSchedule = OCKSchedule(composing: [
+        let strengtheningSchedule = OCKSchedule(composing: [
             OCKScheduleElement(start: beforeBreakfast,
                                end: nil,
                                interval: DateComponents(day: 2),
-                               text: "Anytime throughout the day",
+                               text: "Repeat 10 times",
                                targetValues: [],
                                duration: .allDay)
         ])
 
-        var flueTask = OCKTask(id: CareStoreReferenceManager.TaskIdentifiers.flueEpisodes.rawValue,
-                                   title: "Track Flue",
-                                   carePlanID: nil, schedule: flueSchedule)
+        var strengtheningTask = OCKTask(id: CareStoreReferenceManager.TaskIdentifiers.strengthening.rawValue,
+                                   title: "Sagittal Core Strengthening",
+                                   carePlanID: nil, schedule: strengtheningSchedule)
         
-        coughingTask.instructions = "If you have coughed for a duration of 1 minute or more please log it here."
-        coughingTask.impactsAdherence = false
-        flueTask.instructions = "If you have Flue for a duration of half day or more please log it here."
-        addTask(coughingTask)
-        addTask(flueTask)
+        excerciseTask.instructions = "Read Info."
+        //excerciseTask.impactsAdherence = false
+        excerciseTask.groupIdentifier = "Learn"
+        excerciseTask.userInfo = ["image":"exercisetype.png"]
+        
+        strengtheningTask.instructions = "You can stretch and strengthen the low back muscles that help you stand and lift. Stand with your feet shoulder width apart, about 18” in front of a wall (with your back to the wall).  Hold a household object of desired weight (book, can of soup, exercise weight) directly in front of you. Tighten your abdominal muscles, then reach through your legs to touch the wall, keeping hips and knees bent. Use your hips to push your body back to a standing position, then extend your arms and reach over your head and slightly backward. Repeat 10 times."
+        
+        strengtheningTask.groupIdentifier = "Todo's"
+        addTask(excerciseTask)
+        addTask(strengtheningTask)
     }
 }
--- ORKSample/DailyActivitesViewController.swift
+++ ORKSample/DailyActivitesViewController.swift
@@ -14,4 +14,33 @@ class DailyActivitesViewController: OCKDailyPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func dailyPageViewController(_ dailyPageViewController: OCKDailyPageViewController,
+                                             prepare listViewController: OCKListViewController, for date: Date) {
+           
+           let identifiers = CareStoreReferenceManager.TaskIdentifiers.allCases.map { $0.rawValue }
+           var query = OCKTaskQuery(for: date)
+           query.ids = identifiers
+           query.excludesTasksWithNoEvents = true
+           storeManager.store.fetchAnyTasks(query: query, callbackQueue: .main) { result in
+               guard let tasks = try? result.get() else { return }
+               
+               tasks.forEach { task in
+                   switch task.id {
+                   case CareStoreReferenceManager.TaskIdentifiers.exercisesTypes.rawValue:
+                       let excerciseCard = OCKButtonLogTaskViewController(task: task,
+                                                                         eventQuery: .init(for: date),
+                                                                         storeManager: self.storeManager)
+                       listViewController.appendViewController(excerciseCard, animated: false)
+                       
+                   case CareStoreReferenceManager.TaskIdentifiers.strengthening.rawValue:
+                       let strengtheningCard = OCKButtonLogTaskViewController(task: task,
+                                                                         eventQuery: .init(for: date),
+                                                                         storeManager: self.storeManager)
+                       listViewController.appendViewController(strengtheningCard, animated: false)
+                   default: return
+                   }
+               }
+           }
+       }
 }
