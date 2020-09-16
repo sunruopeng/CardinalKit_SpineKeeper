c55134a01617eaefc6d2a9d417d278f0ca2e07fd
Marco Esquivel <maesqui3@asu.edu>
2020-09-15 23:06:06 -0700

    Updated pod file and configured firebase with application launch in app delegate

--- ORKSample/ORKSample/AppDelegate.swift
+++ ORKSample/ORKSample/AppDelegate.swift
@@ -47,6 +47,7 @@
 
 import UIKit
 import ResearchKit
+import FirebaseCore
 
 import MessageUI
 
@@ -83,6 +84,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UIView.appearance().tintColor = Colors.cardinalRed.color
+        FirebaseApp.configure()
         lockApp()
         return true
     }
